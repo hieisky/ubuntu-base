65a48aca8d106b90504592d2df072fb65ffc341e
hieisky@gmail.com <hieisky@gmail.com>
2016-03-05 14:36:34 +0800

    Update the start script

--- container_start.sh
+++ container_start.sh
@@ -1,1 +1,1 @@
-docker run -p 8080:8080 -v /vagrant/container-share:/vagrant --name nodejs hieisky/ubuntu-base
+docker run -p 49260:22 -v /vagrant/container-share:/vagrant --name nodejs hieisky/ubuntu-base
